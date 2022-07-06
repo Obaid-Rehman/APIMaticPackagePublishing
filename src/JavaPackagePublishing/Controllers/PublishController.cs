4aba4e9d891c034d73413253a69a77f984713ca3
Obaid-Rehman <obaidkhattak@gmail.com>
2022-07-06 19:46:34 +0500

    Add project files.

---
+++ src/JavaPackagePublishing/Controllers/PublishController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.IO.Compression;
+
+namespace JavaPackagePublishing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishController : ControllerBase
+    {
+        private readonly ILogger<PublishController> _logger;
+        private readonly IConfiguration _configuration;
+        public PublishController(ILogger<PublishController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Publish([FromForm] IFormFile data)
+        {
+            if (data == null)
+            {
+                string message = "File can not be null";
+                _logger.LogError(message);
+                return BadRequest(message);
+            }
+            var size = data.Length;
+            if (size == 0)
+            {
+                string message = "File can not be empty";
+                _logger.LogError(message);
+                return BadRequest(message);
+            }
+
+            var sourceFilePath = Path.GetTempFileName();
+            using(var stream = System.IO.File.Create(sourceFilePath))
+            {
+                await data.CopyToAsync(stream);
+            }
+
+            var targetDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+            var zipFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+            var stdStandardOutput = "";
+            var stdErrorOutput = "";
+
+            try
+            {
+                ZipFile.ExtractToDirectory(sourceFilePath, targetDir);
+
+                string mvnInstallationDirectory = _configuration["MAVEN_HOME"];
+                string mvnExecutable = OperatingSystem.IsLinux() ? "mvn" : "mvn.cmd";
+
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = false,
+                    FileName = Path.Combine(mvnInstallationDirectory, mvnExecutable),
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    Arguments = $"-X package -Dmaven.test.skip=true -f {Path.Combine(targetDir, "pom.xml")}"
+                };
+
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    stdStandardOutput = await exeProcess.StandardOutput.ReadToEndAsync();
+                    stdErrorOutput = await exeProcess.StandardError.ReadToEndAsync();
+                    exeProcess.WaitForExit();
+                }
+
+                _logger.LogInformation(stdStandardOutput);
+                if (stdErrorOutput.Length > 0)
+                {
+                    _logger.LogError(stdErrorOutput); 
+                }
+
+                ZipFile.CreateFromDirectory(targetDir, zipFilePath, CompressionLevel.Optimal, false, new ZipEncoder());
+
+                using (var stream = System.IO.File.OpenRead(zipFilePath))
+                {
+                    using (var reader = new BinaryReader(stream))
+                    {
+                        var numBytes = new FileInfo(zipFilePath).Length;
+                        var buff = reader.ReadBytes((int)numBytes);
+                        return File(buff, "application/zip", "response.zip");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Something went wrong. Exeception:\n{e.StackTrace}");
+                return BadRequest(e.StackTrace);
+            }
+            finally
+            {
+                Directory.Delete(targetDir, true);
+                System.IO.File.Delete(sourceFilePath);
+                if (System.IO.File.Exists(zipFilePath))
+                {
+                    System.IO.File.Delete(zipFilePath);
+                }
+            }
+        }
+    }
+}
